4e3a6058284d837fae9aa50c4f0dcf0966694f39
chen834921478 <chen834921478@gmail.com>
2019-03-07 11:03:13 +0800

    菜单管理添加

--- src/configs/menu.js
+++ src/configs/menu.js
@@ -1,29 +1,73 @@
 const menu = [{
-    "id": "menu-article",
-    "text": "游戏管理",
-    "icon": "&#xe616",
-    "children": [{
-            "link": "#",
-            "text": "游戏查询",
-            "dataHref": "game/listPage",
-            "onclick": "Hui_admin_tab(this)"
+    'id': 'menu-article',
+    'text': '游戏管理',
+    'icon': '&#xe616',
+    'children': [{
+            'link': '#',
+            'text': '游戏查询',
+            'dataHref': 'game/listPage',
+            'onclick': 'Hui_admin_tab(this)'
         },
         {
-            "link": "#",
-            "text": "添加游戏",
-            "dataHref": "game/addPage",
-            "onclick": "Hui_admin_tab(this)"
+            'link': '#',
+            'text': '添加游戏',
+            'dataHref': 'game/addPage',
+            'onclick': 'Hui_admin_tab(this)'
         },
 
         {
-            "text": "二级菜单",
-            "children": [{
-                "link": "#",
-                "text": "三级菜单",
-                "dataHref": "_blank.html"
+            'text': '二级菜单',
+            'children': [{
+                'link': '#',
+                'text': '三级菜单',
+                'dataHref': '_blank.html'
             }]
         }
 
     ]
-}]
+},
+{
+    'id': 'menu-article',
+    'text': '物品管理',
+    'icon': '&#xe616',
+    'children': [{
+            // 'link': '#',
+            // 'text': '物品分类-类型',
+            // 'dataHref': 'game/listPage',
+            // 'onclick': 'Hui_admin_tab(this)'
+            'text': '物品分类',
+            'children': [{
+                'link': '#',
+                'text': '物品分类查询',
+                'dataHref': 'game/addPage',
+                'onclick': 'Hui_admin_tab(this)'
+            },{
+                'link': '#',
+                'text': '物品分类添加',
+                'dataHref': 'game/addPage',
+                'onclick': 'Hui_admin_tab(this)'
+            }]
+        },
+        {
+            'link': '#',
+            'text': '物品型号',
+            'dataHref': 'game/addPage',
+            'onclick': 'Hui_admin_tab(this)'
+        },
+        {
+            'link': '#',
+            'text': '物品品质',
+            'dataHref': 'game/addPage',
+            'onclick': 'Hui_admin_tab(this)'
+        },
+        {
+            'link': '#',
+            'text': '物品名称',
+            'dataHref': 'game/addPage',
+            'onclick': 'Hui_admin_tab(this)'
+        },
+
+    ]
+}
+]
 module.exports = menu
